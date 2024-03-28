d67b080984242a597abcfa4c53adee9540a71080
peterxuwei <379411389@qq.com>
2024-03-28 21:41:44 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,21 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "CCTV-1",
+    "CCTV-2",
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-6",
+    "CCTV-13",
+    "湖南卫视",
+    "天津卫视",
+    "浙江卫视",
+     
+]
+favorite_page_num = 9
+default_page_num = 4
+urls_limit = 15
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
